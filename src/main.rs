b7432c131e12c359db2103b31477303a1396ea27
HARRY PRIOR <harryprior@hotmail.co.uk>
2022-08-20 21:17:18 +0100

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
-#![allow(dead_code, unused_variables)]
+#![allow(dead_code, unused_variables, unused_imports)]
 
 use std::{
     collections::{HashMap, HashSet},
     error::Error,
+    fmt::Debug,
     fs::{self, DirEntry, Metadata},
-    os::unix::prelude::MetadataExt,
+    hash::Hash,
+    os::{self, unix::prelude::MetadataExt},
     path::PathBuf,
     process::{exit, Command},
 };
+
+use concat_string::concat_string;
+use itertools::Itertools;
 
 #[derive(Debug)]
 struct File {
@@ -17,13 +22,16 @@ struct File {
 
 #[derive(Debug)]
 struct FileTree {
-    files: Vec<File>,
     dir: Option<String>,
+    files: Vec<File>,
     nodes: Vec<Box<FileTree>>,
 }
 
 impl FileTree {
     fn new(untracked_files: &HashSet<String>, path: &String) -> Option<Self> {
+        // it works but will overflow if file tree too large. No tail recursion. Also really
+        // innefficient to look through every directory
+        // try another impl that create a tree based on only the untracked files
         let dir_info: Vec<(PathBuf, Metadata)> = fs::read_dir(path)
             .unwrap()
             .map(|de| {
@@ -39,6 +47,16 @@ impl FileTree {
             .map(|(p, _)| p.to_str().unwrap().to_owned())
             .collect();
 
+        // recurse until no more sub directories
+        let nodes = if dirs.len() > 0 {
+            dirs.into_iter()
+                .filter_map(|dir| FileTree::new(untracked_files, &dir))
+                .map(|ft| Box::new(ft))
+                .collect()
+        } else {
+            vec![]
+        };
+
         let files: Vec<File> = dir_info
             .iter()
             .filter(|(_, meta)| meta.is_file())
@@ -50,25 +68,82 @@ impl FileTree {
             })
             .collect();
 
-        println!("number of files in {} files: {:?}", path, &files);
-
-
-        if files.len() > 0 || path == "./" {
-            let nodes: Vec<Box<FileTree>> = dirs
-                .into_iter()
-                .filter_map(|dir| FileTree::new(untracked_files, &dir))
-                .map(|ft| Box::new(ft))
-                .collect();
-
-            return Some(Self {
+        if files.len() > 0 || nodes.len() > 0 || path == "./" {
+            Some(Self {
                 files,
                 dir: Some(path.clone()),
                 nodes,
-            });
+            })
+        } else {
+            None
         }
-
-        None
     }
+
+    fn new_fast(files: &Vec<String>) {
+        let paths: Vec<Vec<String>> = files
+            .into_iter()
+            .map(|s| {
+                s.split(std::path::MAIN_SEPARATOR)
+                    .map(|s| s.to_owned())
+                    .scan(String::from(""), |acc, next| {
+                        acc.push(std::path::MAIN_SEPARATOR);
+                        acc.push_str(next.as_str());
+                        Some(acc.to_owned())
+                    })
+                    .collect()
+            })
+            .collect();
+
+
+        let dir_nodes = dir_tree_hierarchy(&paths);
+
+
+
+    }
+}
+
+fn get_files_at_dir(untracked_files: &HashSet<String>, dir_path: &String) -> Vec<File> {
+    fs::read_dir(dir_path)
+        .unwrap()
+        .map(|de| {
+            let path = de.as_ref().unwrap().path();
+            let meta = de.unwrap().metadata().unwrap();
+            (path, meta)
+        })
+        .filter(|(_, meta)| meta.is_file())
+        .map(|(pb, meta)| (pb.to_str().unwrap().to_owned(), meta))
+        .filter(|(fp, _)| untracked_files.contains(&fp[2..]))
+        .map(|(fp, meta)| File {
+            path: fp,
+            byte_size: meta.size(),
+        })
+        .collect()
+}
+
+fn dir_tree_hierarchy<T: Clone + Eq + Hash>(input: &Vec<Vec<T>>) -> Vec<Vec<T>> {
+    let mut output: Vec<HashSet<T>> = vec![];
+    for vec in input.iter() {
+        let vec_length = vec.len();
+        for i in 0..vec_length {
+            // ignore files
+            if i == vec_length - 1 {
+                break;
+            }
+            let input_item = vec[i].clone();
+            if let Some(set) = output.get_mut(i) {
+                set.insert(input_item);
+            } else {
+                let mut new_vec = HashSet::new();
+                new_vec.insert(input_item);
+                output.push(new_vec);
+            }
+        }
+    }
+
+    output
+        .into_iter()
+        .map(|x| x.into_iter().collect())
+        .collect()
 }
 
 fn execute() -> Result<(), Box<dyn Error>> {
@@ -80,9 +155,10 @@ fn execute() -> Result<(), Box<dyn Error>> {
     let std_out = String::from_utf8(cmd.stdout).unwrap();
     let file_list = get_file_list_from(&std_out);
     let file_hash_set: HashSet<String> = file_list.iter().map(|f| f.clone()).collect();
-    let file_tree = FileTree::new(&file_hash_set, &String::from("./"));
-    println!("file tree:");
-    println!("{:?}", file_tree);
+    //let file_tree = FileTree::new(&file_hash_set, &String::from("./"));
+    let file_tree = FileTree::new_fast(&file_list);
+    // println!("file tree:");
+    // println!("{:?}", file_tree);
 
     Ok(())
 }
